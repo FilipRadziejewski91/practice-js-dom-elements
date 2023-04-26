1823354bdae7b7a55810dd8c3f491c6885570188
Filip Radziejewski <hugoboss2048@gmail.com>
2023-04-26 16:38:24 +0200

    task5 completed

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,29 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+const cloneCurr = curr.cloneNode(true);
+const article = curr.parentElement;
+article.appendChild(cloneCurr);
+cloneCurr.innerText = 'Usuń z koszyka'
+
+if (article.hasChildNodes()) {
+    const children = Array.from(article.children);
+    children.forEach(function (item) {
+        if (item !== curr) {
+            item.classList.add('siblings')
+        }
+    })
+}
+
+article.nextElementSibling.title = 'nextElementSibling'
+
+const lastArticle = article.nextElementSibling.nextElementSibling;
+const btn = lastArticle.children[lastArticle.children.length - 1];
+const pElement = document.createElement('p');
+pElement.innerText = 'Jakaś dodatkowa treść';
+
+lastArticle.insertBefore(pElement, btn);
+
+const articleZero = article.cloneNode(true);
+const parentArticle = article.parentElement;
+parentArticle.insertBefore(articleZero, article)
