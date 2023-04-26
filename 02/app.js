77df39f12dce5d59e8caeeb1433d5f96794f4eb0
Filip Radziejewski <hugoboss2048@gmail.com>
2023-04-26 12:38:07 +0200

    task3 completed

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,7 @@
 console.log('DOM');
+
+const element = document.querySelectorAll("[data-url]");
+element.forEach(function (item) {
+    const adres = (item.getAttribute('data-url'));
+    item.setAttribute('href', adres);
+});
--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,19 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parent = document.querySelector('.parent-for-button')
+const newBtn = document.createElement('button');
+parent.appendChild(newBtn);
+
+newBtn.innerText = buttonSettings.text
+
+for (const key in buttonSettings.attr) {
+    const value = buttonSettings.attr[key];
+    newBtn[key] = value;
+}
+
+for (const key in buttonSettings.css) {
+    const value = buttonSettings.css[key];
+    newBtn.style[key] = value;
+}
